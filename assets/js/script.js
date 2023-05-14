262597a6ac21853993d33611d48399b0f1a2ea1c
jamalandan <jamalandan@outlook.com>
2023-05-14 16:56:08 +0700

    first upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// typed js home section
+var typed = new Typed("#typed", {
+  strings: ["Frontend Developer", "Graphic Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 200,
+  startDelay: 100,
+  fadeOut: false,
+  loop: true,
+});
